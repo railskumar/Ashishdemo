f48d523e33c705167bed44c6a10744bd63598c0b
rails <rails@rails-dev.(none)>
2014-01-09 16:42:00 +0530

    add devise and bootstrap3 work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ashishdemo::Application.config.secret_key_base = '28fe1b1c87ec32fb409a18b9089b5504a3aff3778ea9864b1a86001f7022c74e607625e4c516ea4043995aaf35bff8e0f161fcea91ae9fdd7551862090430b59'
